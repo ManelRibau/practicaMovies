e1a0ee53a4e2d2d33b33c2607466f4f3ff1d57e8
ManelRibau <manel.ribau94@gmail.com>
2019-10-02 17:07:09 +0200

    Second commit to the personal git account

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('sci-fi/', views.movie_sci_fi, name='sci-fi'),
+    path('action/', views.movie_action, name='action'),
+    path('adventures/', views.movie_adventures, name='adventures'),
+    path('drama/', views.movie_drama, name='drama'),
+    path('romance/', views.movie_romance, name='romance'),
+    path('comedy/', views.movie_comedy, name='comedy'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Movie
 
 def movie_list(request):
     return render(request, 'movie/movie_list.html', {})
+
+def movie_sci_fi(request):
+    movies = Movie.objects.filter(genre__startswith='Ciencia')
+    return render(request, 'movie/sci-fi.html', {'movies': movies})
+
+def movie_action(request):
+    movies = Movie.objects.filter(genre__startswith='Accio')
+    return render(request, 'movie/action.html', {'movies': movies})
+
+def movie_adventures(request):
+    movies = Movie.objects.filter(genre__startswith='Aventur')
+    return render(request, 'movie/adventures.html', {'movies': movies})
+
+def movie_comedy(request):
+    movies = Movie.objects.filter(genre__startswith='Comedia')
+    return render(request, 'movie/comedy.html', {'movies': movies})
+
+def movie_drama(request):
+    movies = Movie.objects.filter(genre__startswith='Drama')
+    return render(request, 'movie/drama.html', {'movies': movies})
+
+def movie_romance(request):
+    movies = Movie.objects.filter(genre__startswith='Romanti')
+    return render(request, 'movie/romance.html', {'movies': movies})
